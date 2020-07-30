5a94bd7b59d9333846950636f439f38c44d0b4ad
spcjw <30407259+spcjw@users.noreply.github.com>
2020-07-30 15:38:42 +0900

    프로젝트 세팅

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const port = process.env.PORT || 3000;
+
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve("./dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          process.env.NODE_ENV === "production"
+            ? MiniCssExtractPlugin // 프로덕션 환경
+            : "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_moduels/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        loader: "url-loader",
+        options: {
+          name: "[name].[ext]?[hash]",
+          limit: 10000, //10kb
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `빌드 날짜 : ${new Date().toLocaleString()}`,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      minify:
+        process.env.NODE_ENV === "production"
+          ? {
+              collapseWhiteSpace: true, // 빈칸 제거
+              removeComments: true, // 주석 제거
+            }
+          : false,
+      hash: process.env.NODE_ENV === "production",
+    }),
+    new CleanWebpackPlugin(),
+    ...(process.env.NODE_ENV === "production"
+      ? [new MiniCssExtractPlugin({ filename: `[name].css` })]
+      : []),
+  ],
+  devServer: {
+    host: "localhost",
+    port: port,
+    open: true,
+  },
+};
