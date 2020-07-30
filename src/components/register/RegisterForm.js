ca4bb8912c4ceffd6a2918e32c76ec1d89cab558
spcjw <30407259+spcjw@users.noreply.github.com>
2020-07-30 21:02:27 +0900

    PC-6 회원가입 폼 데이터 작업중

--- src/components/register/RegisterForm.js
+++ src/components/register/RegisterForm.js
@@ -1,7 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
+import useInput from "../../lib/hooks/useInput";
 
-function RegisterForm() {
-  return <div>RegisterForm</div>;
+function RegisterForm(props) {
+  // const [inputs, setInputs] = useState({
+  //   email: "",
+  //   name: "",
+  //   password: "",
+  //   passwordCheck: "",
+  //   image: "",
+  //   nativeLanguage: "",
+  //   targetLanguage: "",
+  //   gender: "",
+  // });
+
+  const { onSubmit, onChange } = props;
+
+  const {
+    email,
+    name,
+    password,
+    passwordCheck,
+    image,
+    nativeLanguage,
+    targetLanguage,
+    gender,
+  } = props.inputs;
+
+  // const onChange = (e) => {
+  //   if (
+  //     e.target.name === "nativeLanguage" ||
+  //     e.target.name === "targetLanguage" ||
+  //     e.target.name === "gender"
+  //   ) {
+  //     console.log(e.target);
+  //   } else {
+  //     setInputs({
+  //       ...inputs,
+  //       [e.target.name]: e.target.value,
+  //     });
+  //   }
+  // };
+
+  return (
+    <div>
+      <h2>회원가입</h2>
+      <form onSubmit={onSubmit}>
+        <div className="row">
+          <span>이메일</span>
+          <input type="text" name="email" value={email} onChange={onChange} />
+        </div>
+        <div className="row">
+          <span>이름(서비스 내에서 사용됩니다.)</span>
+          <input type="text" name="name" value={name} onChange={onChange} />
+        </div>
+        <div className="row">
+          <span>비밀번호</span>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={onChange}
+          />
+        </div>
+        <div className="row">
+          <span>비밀번호 확인</span>
+          <input
+            type="password"
+            name="passwordCheck"
+            value={passwordCheck}
+            onChange={onChange}
+          />
+        </div>
+        <div className="row">
+          <span>모국어</span>
+          <select
+            name="nativeLanguage"
+            value={nativeLanguage}
+            onChange={onChange}
+          >
+            <option value="kr">KR</option>
+            <option value="en">EN</option>
+          </select>
+        </div>
+        <div className="row">
+          <span>학습언어</span>
+          <select
+            name="targetLanguage"
+            value={targetLanguage}
+            onChange={onChange}
+          >
+            <option value="kr">KR</option>
+            <option value="en">EN</option>
+          </select>
+        </div>
+        <div className="row">
+          <span>성별</span>
+          <select name="gender" value={gender} onChange={onChange}>
+            <option value="m">남자</option>
+            <option value="f">여자</option>
+          </select>
+        </div>
+        <button type="submit">완료</button>
+      </form>
+    </div>
+  );
 }
 
 export default RegisterForm;
--- src/containers/register/RegisterFormContainer.js
+++ src/containers/register/RegisterFormContainer.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import RegisterForm from "../../components/register/RegisterForm";
 
 function RegisterFormContainer() {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+    // 서버로 axios로 요청
+    /**
+     * 1. 비밀번호, 비밀번호 확인 같은지, validation 체크
+     * 2. 이메일 validation 체크
+     * 3. 각 항목 비어있는지 체크
+     * 4. 프로필사진 업로드
+     * 5. 이메일 중복체크
+     * 6. css
+     */
+  };
+
+  const [inputs, setInputs] = useState({
+    email: "",
+    name: "",
+    password: "",
+    passwordCheck: "",
+    image: "",
+    nativeLanguage: "",
+    targetLanguage: "",
+    gender: "",
+  });
+
+  const onChange = (e) => {
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div>
-      <RegisterForm />
+      <RegisterForm onSubmit={onSubmit} inputs={inputs} onChange={onChange} />
     </div>
   );
 }
