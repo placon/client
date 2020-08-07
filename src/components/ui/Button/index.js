c8e4057aaeaec11751035a777b0c7626ebf6acf1
jungwone <30407259+spcjw@users.noreply.github.com>
2020-08-07 21:32:10 +0900

    PC-26 버튼 컴포넌트 #done

--- src/components/ui/Button/index.js
+++ src/components/ui/Button/index.js
@@ -2,12 +2,22 @@ import React from "react";
 import classNames from "classnames";
 import "./index.scss";
 
-function Button({ children, size }) {
-  return <button className={classNames("Button", size)}>{children}</button>;
+function Button({ children, size, color, outline, fullWidth, ...rest }) {
+  return (
+    <button
+      className={classNames("Button", size, color, { outline, fullWidth })}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
 }
 
 Button.defaultProps = {
   size: "medium",
+  color: "blue",
+  outline: false,
+  fullWidth: false,
 };
 
 export default Button;
