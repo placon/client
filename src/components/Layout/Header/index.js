4922f173a24169af7ae547363deb79f786498160
jungwone <30407259+spcjw@users.noreply.github.com>
2020-08-10 20:35:11 +0900

    PC-36 헤더 작업. 로그인, 비로그인 분류

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -1,14 +1,56 @@
 import React from "react";
 import "./index.scss";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import useInput from "../../../lib/hooks/useInput";
+import Button from "../../UI/Button";
 
 function index() {
+  const { isLoggedIn, myInfo } = useSelector((state) => state.user); // 로그인 되어있는지 체크
+  // const isLoggedIn = true;
+  // const myInfo = true;
+  const [searchKeyword, onChangeSearchKeyword] = useInput("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("키워드 : ", searchKeyword);
+  };
+
   return (
     <header className="header-container">
       <div className="inner">
-        <div className="left">PLACON</div>
+        <div className="left">
+          <Link to="/">P L A C O N .</Link>
+        </div>
+        <form className="middle" onSubmit={onSubmit}>
+          <input
+            className="search-input"
+            type="text"
+            value={searchKeyword}
+            onChange={onChangeSearchKeyword}
+          />
+          <button className="magnifier" type="submit"></button>
+        </form>
         <div className="right">
-          <figure className="magnifier"></figure>
-          <figure className="mailbox"></figure>
+          {isLoggedIn && myInfo ? (
+            <>
+              <figure className="mailbox"></figure>
+              <figure
+                className={`profile ${
+                  !myInfo.profile_image && "default-image"
+                }`}
+              ></figure>
+            </>
+          ) : (
+            <>
+              <Link to="/login">
+                <Button size="small">로그인</Button>
+              </Link>
+              <Link to="/signup">
+                <Button size="small">회원가입</Button>
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </header>
