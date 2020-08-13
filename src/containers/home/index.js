c4cb057109a0fd8bc16303850a8d7e4bc91dc8eb
jungwone <30407259+spcjw@users.noreply.github.com>
2020-08-13 13:39:09 +0900

    css 및 포스트 리스트 관련해서 수정

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "../../components/home";
+import PostList from "../../components/Post/PostList";
 
 function HomeContainer() {
   return (
     <>
       <Home />
+      <PostList />
     </>
   );
 }
