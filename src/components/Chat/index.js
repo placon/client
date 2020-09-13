509a182d7bf64419fa5b2796a33ebbcafc0c4253
jungwone <30407259+spcjw@users.noreply.github.com>
2020-09-13 17:44:26 +0900

    PC-90 작업중

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./index.scss";
 
 function Chat(props) {
-  console.log(props.userInfo);
+  useEffect(() => {
+    const myInfo = JSON.parse(window.sessionStorage.getItem("myInfo"));
+    if (props.userInfo._id !== myInfo._id) {
+      // TODO : 메소드 실행해야됨 채팅방 생성/조회
+    }
+  }, []);
   return (
     <div className="ChatPageComponent">
       <div>sdfsdf</div>
