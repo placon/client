9c6fa9834aa5ab8d65faff22513d0a3b03350518
spcjw <prsjb77@gmail.com>
2020-09-19 17:24:38 +0900

    PC-90 작업중

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,12 +1,25 @@
 import React, { useEffect, useState } from "react";
 import ChatRoom from "./ChatRoom";
 import "./index.scss";
+import io from "socket.io-client";
+import Input from "./Input";
+
+let socket;
 
 function Chat(props) {
+  const ENDPOINT = "localhost:3000";
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [users, setUsers] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
   useEffect(() => {
     const myInfo = JSON.parse(window.sessionStorage.getItem("myInfo"));
     if (props.userInfo._id !== myInfo._id) {
       // TODO : 메소드 실행해야됨 채팅방 생성/조회
+      socket = io(ENDPOINT);
+      // 방, 아이디 세팅
     }
   }, []);
   return (
